2a292ef4df1c0bab6ee6560d44922d258b3b3923
Jagjit <jagjitsinghqy@gmail.com>
2020-10-06 12:48:57 +0530

    Basic_Oper

---
+++ src/dataSchema.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const dataSchema = new mongoose.Schema({
+    data: {
+        type: String,
+        required: true
+    }
+});
+
+const Data = mongoose.model('Data',dataSchema);
+
+export default Data;
